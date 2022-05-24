0b46bd4dd2a9fbb16b484a20be1e4d2f22e53310
Diogo Saraiva <diosaraiva@hotmail.com>
2022-05-24 17:59:04 -0300

    Blockchain studies.

--- src/main/java/com/diosaraiva/springutils/ui/swing/SwingMain.java
+++ src/main/java/com/diosaraiva/springutils/ui/swing/SwingMain.java
@@ -3,6 +3,9 @@ package com.diosaraiva.springutils.ui.swing;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JMenu;
 
 public class SwingMain{
 	private JFrame frame;
@@ -12,13 +15,27 @@ public class SwingMain{
 	 */
 	public void launch(){
 		frame = new JFrame();
-
-		JLabel lblText = new JLabel("Hello World!", SwingConstants.CENTER);
-		frame.getContentPane().add(lblText);
-
 		frame.setSize(640,480);
-		
-		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-	}  
+		
+		JMenuBar menuBar = new JMenuBar();
+		frame.setJMenuBar(menuBar);
+		
+		JMenu menuFile = new JMenu("File");
+		menuBar.add(menuFile);
+		
+		JMenuItem menuItemExit = new JMenuItem("Exit");
+		menuFile.add(menuItemExit);
+		
+		JMenu menuHelp = new JMenu("Help");
+		menuBar.add(menuHelp);
+		
+		JMenuItem menuItemAbout = new JMenuItem("About");
+		menuHelp.add(menuItemAbout);
+		
+		JLabel labelText = new JLabel("Hello World!", SwingConstants.CENTER);
+		frame.getContentPane().add(labelText);
+
+		frame.setVisible(true);
+	}
 }
